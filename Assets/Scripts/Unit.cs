22122a7054a8078b2a0c15ed43d65864ed37fef1
J8rgen <jorgenvarjas@gmail.com>
2024-06-28 13:00:09 +0300

    Unit animator, parameters

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour {
 
+    [SerializeField] private Animator unitAnimator; // object with visual, animator
+
+    //private const string IS_WALKING = "IsWalking";
+
     private Vector3 targetPosition;
 
     private void Update() {
+
 
         float stoppingDistance = .1f; // distance it is fine to stop at, otherwise will keep trying to correct the distance
 
@@ -15,6 +20,11 @@ public class Unit : MonoBehaviour {
             Vector3 moveDirection = (targetPosition - transform.position).normalized; // target - current position (move direction vector)
             float moveSpeed = 4f;
             transform.position += moveDirection * moveSpeed * Time.deltaTime; // framerate independent
+
+            unitAnimator.SetBool("IsWalking", true); // set animation
+        }
+        else {
+            unitAnimator.SetBool("IsWalking", false); // set animation
         }
 
 
